f61b2cb02b732c2132439e30cdb1ba620a8f1c9d
LXNA <beljohnluna@gmail.com>
2025-06-28 09:12:28 +0800

    fix

--- api/zoho-token.js
+++ api/zoho-token.js
@@ -1,7 +1,8 @@
 export default async function handler(req, res) {
-  const CLIENT_ID = "YOUR_CLIENT_ID";
-  const CLIENT_SECRET = "YOUR_CLIENT_SECRET";
-  const REFRESH_TOKEN = "YOUR_REFRESH_TOKEN";
+  const CLIENT_ID = "1000.STJU7BGXGZLLC25TFXJNFPVULA62KD";
+  const CLIENT_SECRET = "f530306ab0be6a03f3f9f05b622f806d09b577ea7b";
+  const REFRESH_TOKEN =
+    "1000.77ccce891d8d2dad76ce87ceeea2bd23.39df35043a5a2f9dbbbbc42c432a2e7e";
 
   const body = new URLSearchParams({
     refresh_token: REFRESH_TOKEN,
