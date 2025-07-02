f38504abe05d8285ab9508059177618cc19af5db
LXNA <beljohnluna@gmail.com>
2025-07-02 21:14:59 +0800

    updated refresh token

--- api/zoho-token.js
+++ api/zoho-token.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const CLIENT_ID = "1000.STJU7BGXGZLLC25TFXJNFPVULA62KD";
   const CLIENT_SECRET = "f530306ab0be6a03f3f9f05b622f806d09b577ea7b";
   const REFRESH_TOKEN =
-    "1000.59079b74bd62ea86c8c95ec94be90deb.eed14e4a3b4fa9f64b20d65111d82d2d";
+    "1000.8dbc396d3261e72d35653aa47a98f5db.471164397caa9970337e126cb454fb7e";
   // old "1000.77ccce891d8d2dad76ce87ceeea2bd23.39df35043a5a2f9dbbbbc42c432a2e7e";
 
   const body = new URLSearchParams({
